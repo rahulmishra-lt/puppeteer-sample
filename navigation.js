e8a716d1356283b066a975912119829cc95fa1a6
Rahul Mishra <rahulmishra@lambdatest.com>
2023-11-16 19:10:30 +0530

    addded: retry for failed test

--- navigation.js
+++ navigation.js
@@ -1,9 +1,10 @@
-'use strict';
-const { strict } = require('once');
 const puppeteer = require('puppeteer');
-const expect = require('chai').expect;
 
-(async () => {    
+(async () => {
+    const maxRetries = 2;
+    const retryDelay = 1000;
+    let success = false;
+
     const capabilities = {
         'browserName': 'Chrome',
         'browserVersion': 'latest',
@@ -16,34 +17,44 @@ const expect = require('chai').expect;
             'accessKey': process.env.LT_ACCESS_KEY || "Your Access Key",
             'network': true
         }
-   };
-    
-    try {
-        const browser = await puppeteer.connect({
-            browserWSEndpoint:
-                `wss://cdp.lambdatest.com/puppeteer?capabilities=${encodeURIComponent(JSON.stringify(capabilities))}`,
-        });
+    };
 
-        const page = await browser.newPage();
-        await page.setViewport({
-            width: 1024,
-            height: 768,
-            deviceScaleFactor: 1,
-          });
-        console.log("Navigating to LambdaTest");
-        await page.goto('https://www.lambdatest.com/');
-        console.log("Navigating to Pricing");
-        await page.goto('https://www.lambdatest.com/pricing');
-        console.log("Navigating to Automation");
-        await page.goto('https://www.lambdatest.com/automation-testing');
+    for (let retryCount = 0; retryCount < maxRetries && !success; retryCount++) {
+        try {
+            const browser = await puppeteer.connect({
+                browserWSEndpoint:
+                    `wss://cdp.lambdatest.com/puppeteer?capabilities=${encodeURIComponent(JSON.stringify(capabilities))}`,
+            });
 
-        await page.evaluate(_ => {}, `lambdatest_action: ${JSON.stringify({ action: 'setTestStatus', arguments: { status: 'passed', remark: "Test Passed" } })}`)
+            const page = await browser.newPage();
+            await page.setViewport({
+                width: 1024,
+                height: 768,
+                deviceScaleFactor: 1,
+            });
+            console.log("Navigating to LambdaTest");
+            await page.goto('https://www.lambdatest.com/');
+            console.log("Navigating to Pricing");
+            await page.goto('https://www.lambdatest.com/pricing');
+            console.log("Navigating to Automation");
+            await page.goto('https://www.lambdatest.com/automation-testing');
 
-        console.log("Closing browser");
-        await browser.close();
+            await page.evaluate(_ => { }, `lambdatest_action: ${JSON.stringify({ action: 'setTestStatus', arguments: { status: 'passed', remark: "Test Passed" } })}`)
 
-    } catch (e) {
-        await page.evaluate(_ => {}, `lambdatest_action: ${JSON.stringify({ action: 'setTestStatus', arguments: { status: 'failed', remark: "Test Failed" } })}`)
-        console.log("Error - ", e);
+            console.log("Closing browser");
+            await browser.close();
+
+            success = true;
+        } catch (error) {
+            console.error(`Test failed on attempt ${retryCount + 1}: ${error}`);
+            if (retryCount < maxRetries - 1) {
+                console.log(`Retrying test in ${retryDelay} milliseconds...`);
+                await new Promise((resolve) => setTimeout(resolve, retryDelay));
+            }
+        }
+    }
+
+    if (!success) {
+        throw new Error('Test failed after all retries');
     }
 })();
